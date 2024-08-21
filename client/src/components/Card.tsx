5ea956877371fb0afd4d493ec274fbd8b673951f
idzibart <idzi.bart@gmail.com>
2024-08-21 12:25:10 +0200

    card commit

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -16,7 +16,36 @@ const Card = ({ item }: CardProps) => {
           className="h-full w-full rounded-lg border object-cover"
         />
       </Link>
-      <div className="w-3/5">Tekst</div>
+      <div className="w-3/5">
+        <h2>
+          <Link to={`${item.id}`}>{item.title}</Link>
+        </h2>
+        <p>
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p>$ {item.price}</p>
+        <div>
+          <div>
+            <div>
+              <img src="/bed.png" alt="" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div>
+              <img src="/bath.png" alt="" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src="/save.png" alt="" />
+            </div>
+            <div>
+              <img src="/.chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
